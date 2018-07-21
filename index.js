e4fa322ad256db0597bd501eef52d272bee60e29
Expert <expert.developer.it@gmail.com>
2018-07-21 16:54:15 +0530

    path

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
 const mongo_db = process.env.SHOPIFY_MONGO_DB;
 const scopes = 'read_content,write_content,read_themes,write_themes';
-const forwardingAddress = "https://shopify1.llank.com"; // Replace this with your HTTPS Forwarding address
+const forwardingAddress = process.env.APP_URL; // Replace this with your HTTPS Forwarding address
 
 // cron.schedule('* */1 * * *', function(){
 //   cronJob.setupSettings();
